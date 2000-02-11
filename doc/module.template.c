da2c9c2e8e9bae9b1fbf6db5063d0de61d4dad29
andrej <andrej>
2000-02-11 23:11:56 +0000

    Initial revision

---
+++ doc/module.template.c
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2000  Andrej N. Gritsenko <andrej@rep.kiev.ua>
+ *
+ *     This program is free software; you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation; either version 2 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program; if not, write to the Free Software
+ *     Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * The FoxEye module template.
+ */
+
+#include "foxeye.h"
+#include "modules.h"
+
+/*
+ * this function must receive signals:
+ *  S_TERMINATE - unload module,
+ *  S_REPORT - out state info to log.
+ */
+static int module_signal (INTERFACE *iface, ifsig_t sig)
+{
+    return 0;
+}
+
+/*
+ * this function called when you load a module.
+ * Input: parameters string args.
+ * Returns: address of signals receiver function, NULL if not loaded.
+ */
+Function ModuleInit (char *args)
+{
+    return (&module_signal);
+}
