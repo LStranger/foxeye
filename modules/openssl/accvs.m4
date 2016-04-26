9f3e2a1bae80626eae3707b48fce81a809af38af
Andriy Grytsenko <andrej@rep.kiev.ua>
2016-04-26 03:58:32 +0300

    New module 'openssl' to handle connects using OpenSSL.

---
+++ modules/openssl/accvs.m4
@@ -0,0 +1,225 @@
+dnl OpenSSL support from configure.in for rusnet-ircd-1.5, reworked
+
+fe_save_LIBS="$LIBS"
+fe_save_LDFLAGS="$LDFLAGS"
+fe_save_CFLAGS="$CFLAGS"
+USE_OPENSSL=no
+LIBS="-lcrypto -lssl $LIBS"
+openssl_libpath=
+openssl_include=
+
+AC_ARG_WITH(openssl,
+    AC_HELP_STRING([--with-openssl=PATH], [Specify path to OpenSSL installation]), [
+    fe_openssl_path=$withval], [
+    fe_openssl_path=auto])
+
+dnl Search for OpenSSL library
+case $fe_openssl_path in
+no)
+    # Disable OpenSSL
+    ;;
+
+auto)
+    # Autodetect
+    AC_MSG_CHECKING(for OpenSSL libraries in default paths)
+    AC_LINK_IFELSE([AC_LANG_CALL([], [SSL_library_init])], [
+	AC_MSG_RESULT(found)
+	# Found in default paths
+	USE_OPENSSL=yes
+    ], [
+	AC_MSG_RESULT(no)
+	# Search for it
+	for tryssl_dir in	/usr/local/ssl /usr /usr/local /usr/local/share \
+				/usr/local/openssl /usr/lib/openssl /usr/lib/ssl \
+				/usr/pkg /opt /opt/openssl
+	do
+	    test -d "$tryssl_dir/." || continue
+	    if test -d "${tryssl_dir}/lib"; then
+		openssl_libpath="${tryssl_dir}/lib"
+	    else
+		openssl_libpath="${tryssl_dir}"
+	    fi
+	    LDFLAGS="-L${openssl_libpath} ${fe_save_LDFLAGS}"
+	    if test -d "${tryssl_dir}/include"; then
+		openssl_include="${tryssl_dir}/include"
+	    else
+		openssl_include="${tryssl_dir}"
+	    fi
+	    # Now check if it links
+	    unset ac_cv_lib_ssl_SSL_library_init
+	    AC_MSG_CHECKING(for OpenSSL libraries in $tryssl_dir)
+	    AC_LINK_IFELSE([AC_LANG_CALL([], [SSL_library_init])], [
+		AC_MSG_RESULT(found)
+		USE_OPENSSL=yes
+		break
+	    ], [
+		AC_MSG_RESULT(no)
+		openssl_include=
+	    ])
+	done
+    ])
+    dnl at this point if $USE_OPENSSL isn't "yes" then it's not found
+    if test x$USE_OPENSSL != xyes; then
+	AC_MSG_WARN(could not find OpenSSL libcrypto: see config.log for details)
+	openssl_libpath=
+    fi
+    ;;
+
+yes)
+    # Try to enforce link
+    AC_MSG_CHECKING(for OpenSSL libraries in default paths)
+    AC_LINK_IFELSE([AC_LANG_CALL([], [SSL_library_init])], [
+	AC_MSG_RESULT(found)
+	USE_OPENSSL=yes
+    ], [
+	AC_MSG_RESULT(no)
+	AC_MSG_FAILURE(couldn't find OpenSSL)
+    ])
+    ;;
+
+*)
+    # Try to find it in the given path
+    case "$fe_openssl_path" in
+    # Relative paths
+    ./*|../*)
+	fe_openssl_path="`pwd`/$fe_openssl_path"
+	;;
+    esac
+    if test -d "$fe_openssl_path/lib"; then
+	openssl_libpath="$fe_openssl_path/lib"
+    else
+	openssl_libpath="$fe_openssl_path"
+    fi
+    LDFLAGS="-L${openssl_libpath} ${fe_save_LDFLAGS}"
+    if test -d "$fe_openssl_path/include"; then
+	openssl_include="$fe_openssl_path/include"
+    else
+	openssl_include="$fe_openssl_path"
+    fi
+
+    # Now check if it links
+    AC_MSG_CHECKING(for OpenSSL libraries in $openssl_libpath)
+    AC_LINK_IFELSE([AC_LANG_CALL([], [SSL_library_init])], [
+	AC_MSG_RESULT(found)
+	USE_OPENSSL=yes
+    ], [
+	AC_MSG_RESULT(no)
+	AC_MSG_FAILURE(couldn't find OpenSSL)
+    ])
+    ;;
+esac
+dnl -- end of OpenSSL search
+
+if test x$USE_OPENSSL = xyes; then
+    dnl Check for OpenSSL headers
+    AC_CHECK_HEADER([openssl/ssl.h], [], [
+	if test -z "$openssl_include"; then
+	    AC_MSG_FAILURE(couldn't find OpenSSL)
+	else
+	    OPENSSL_CFLAGS="-I$openssl_include"
+	    CFLAGS="${OPENSSL_CFLAGS} ${CFLAGS}"
+	    AC_MSG_CHECKING(in $openssl_include)
+	    unset ac_cv_header_openssl_ssl_h
+	    AC_CHECK_HEADER([openssl/ssl.h], [], [
+		AC_MSG_FAILURE(couldn't find OpenSSL)
+	    ])
+	fi
+    ])
+
+    dnl Determine OpenSSL header version
+    AC_MSG_CHECKING(OpenSSL header version)
+    AC_RUN_IFELSE([
+	AC_LANG_SOURCE([[
+#include <stdio.h>
+#include <string.h>
+#include <openssl/opensslv.h>
+#define DATA "conftest.sslincver"
+int main(void) {
+        FILE *fd;
+        int rc;
+
+        fd = fopen(DATA,"w");
+        if(fd == NULL)
+                exit(1);
+        if ((rc = fprintf(fd ,"%x (%s)\n", OPENSSL_VERSION_NUMBER, OPENSSL_VERSION_TEXT)) <0)
+                exit(1);
+        exit(0);
+}
+    ]])], [
+	ssl_header_ver=`cat conftest.sslincver`
+	AC_MSG_RESULT($ssl_header_ver)
+    ], [
+	AC_MSG_RESULT(not found)
+	AC_MSG_ERROR(OpenSSL version header not found.)
+    ], [
+	AC_MSG_WARN(cross compiling: not checking)
+    ])
+
+    dnl Determine OpenSSL library version
+    AC_MSG_CHECKING(OpenSSL library version)
+    AC_RUN_IFELSE([
+	AC_LANG_SOURCE([[
+#include <stdio.h>
+#include <string.h>
+#include <openssl/opensslv.h>
+#include <openssl/crypto.h>
+#define DATA "conftest.ssllibver"
+int main(void) {
+        FILE *fd;
+        int rc;
+
+        fd = fopen(DATA,"w");
+        if(fd == NULL)
+                exit(1);
+
+        if ((rc = fprintf(fd ,"%x (%s)\n", SSLeay(), SSLeay_version(SSLEAY_VERSION))) <0)
+                exit(1);
+
+        exit(0);
+}
+    ]])], [
+	ssl_library_ver=`cat conftest.ssllibver`
+	AC_MSG_RESULT($ssl_library_ver)
+    ], [
+	AC_MSG_RESULT(not found)
+	AC_MSG_ERROR(OpenSSL library not found.)
+    ], [
+	AC_MSG_WARN(cross compiling: not checking)
+    ])
+
+    dnl Sanity check OpenSSL headers
+    AC_MSG_CHECKING(whether OpenSSL headers match the library)
+    AC_RUN_IFELSE([
+	AC_LANG_SOURCE([[
+#include <string.h>
+#include <openssl/opensslv.h>
+int main(void) { exit(SSLeay() == OPENSSL_VERSION_NUMBER ? 0 : 1); }
+    ]])], [
+	AC_MSG_RESULT(yes)
+    ], [
+	AC_MSG_RESULT(no)
+	AC_MSG_FAILURE(your OpenSSL headers do NOT match your library)
+    ], [
+	AC_MSG_WARN(cross compiling: not checking)
+    ])
+
+    AC_DEFINE([USE_OPENSSL], [1], [Define if openssl package must be used for compilation/linking.])
+fi
+
+LIBS="$fe_save_LIBS"
+CFLAGS="$fe_save_CFLAGS"
+LDFLAGS="$fe_save_LDFLAGS"
+
+if test -n "$OPENSSL_CFLAGS"; then
+    CPPFLAGS="$OPENSSL_CFLAGS $CPPFLAGS"
+fi
+openssl_libs="-lssl -lcrypto"
+if test -n "$openssl_libpath"; then
+    openssl_libs="$openssl_libs -L$openssl_libpath -R$openssl_libpath"
+fi
+if test "$fe_cv_static" = yes; then
+    STATICLIBS="${openssl_libs} ${STATICLIBS}"
+else
+    MODLIBS="MODLIBS_openssl=\"${openssl_libs}\" ${MODLIBS}"
+fi
+# End of OpenSSL
---
+++ modules/openssl/openssl.c
@@ -0,0 +1,480 @@
+/*
+ * Copyright (C) 2016  Andrej N. Gritsenko <andrej@rep.kiev.ua>
+ *
+ *     This program is free software; you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation; either version 2 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc.,
+ *     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * This file is a part of FoxEye 'openssl' module.
+ */
+
+#include "foxeye.h"
+
+#ifdef USE_OPENSSL
+
+#include "modules.h"
+#include "init.h"
+#include "socket.h"
+#include "direct.h"
+#include "sheduler.h"
+
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+#include <openssl/engine.h>
+#include <openssl/conf.h>
+
+struct sslbuff {
+  BIO *bio;
+  size_t bufptr, inbuf;
+  char buf[2*MB_LEN_MAX*MESSAGEMAX];
+};
+
+struct connchain_buffer {
+  struct peer_t *peer;
+  struct connchain_i *saved_chain;
+  struct connchain_buffer *next;
+  ssize_t error;
+  SSL *ssl;
+  struct sslbuff in, out;
+};
+
+/* global list of buffers, need it in case of module termination */
+static struct connchain_buffer *sslbuflist = NULL;
+
+static SSL_CTX *ctx = NULL;
+static bool _initialized = FALSE;
+
+static char ssl_certificate_file[PATH_MAX+1];
+static char ssl_key_file[PATH_MAX+1];
+
+static void _freesslbuff(struct connchain_buffer **buf)
+{
+  struct connchain_buffer **sb;
+
+  for (sb = &sslbuflist; *sb; sb = &(*sb)->next)
+    if (*sb == *buf)
+      break;
+  if (*sb != NULL)
+    *sb = (*buf)->next;
+  else
+    ERROR("openssl: cannot find buffer %p in list to free it!", *buf);
+  DBG("openssl: freeing buffer %p", *buf);
+//  BIO_free((*buf)->in.bio);
+//  BIO_free((*buf)->out.bio);
+  SSL_free((*buf)->ssl);
+  FREE(buf);
+}
+
+static void _s_check_saved_buffer(idx_t id, struct connchain_buffer *buf)
+{
+  ssize_t i, o;
+
+  /* check if there is new data to pull from buffers into buf->in.buf */
+  if (buf->in.inbuf < sizeof(buf->in.buf))
+    i = Connchain_Get(&buf->saved_chain, -1, &buf->in.buf[buf->in.inbuf],
+		      (sizeof(buf->in.buf) - buf->in.inbuf));
+  else
+    i = 0;
+  if (i > 0) {
+    buf->in.inbuf += i;
+    dprint(6, "openssl: found stream data: size=%zd", i);
+  }
+  /* check if there is old data left to push from buffers into socket */
+  o = 0;
+  o = Connchain_Put(&buf->saved_chain, id, NULL, &o);
+  /* kill chain when done */
+  if (i < 0 && o < 0 && Connchain_Get(&buf->saved_chain, id, NULL, 0))
+    buf->saved_chain = NULL;
+  DBG("openssl: cleared old chain");
+}
+
+/* does buffer part of _ccfilter_S_send() job */
+static ssize_t _ssl_try_send_buffers(struct connchain_i **ch, idx_t id,
+				     struct connchain_buffer *buf, bool check_bio)
+{
+  size_t so;
+  ssize_t i;
+
+  if (check_bio) {
+    i = BIO_read(buf->out.bio, &buf->out.buf[buf->out.inbuf],
+		 sizeof(buf->out.buf) - buf->out.inbuf);
+    if (i > 0)
+      buf->out.inbuf += i;
+    /* else
+      DBG("openssl: BIO_read error code %zd", i); */
+  }
+  so = buf->out.inbuf - buf->out.bufptr;
+  if (so == 0)			/* nothing to send */
+    return 0;
+  i = Connchain_Put(ch, id, &buf->out.buf[buf->out.bufptr], &so);
+  /* DBG("openssl: tried to send data, size=%zu sent=%zd", so, i); */
+  if (i < 0)
+    return (i);
+  if (i > 0)
+    dprint(6, "openssl: sent encrypted data, size=%zd", i);
+  if (so == 0)			/* done */
+    buf->out.bufptr = buf->out.inbuf = 0;
+  else
+    buf->out.bufptr += i;
+  return i;
+}
+
+/* does chain part of _ccfilter_S_recv() job */
+static void _ssl_check_input_from_chain(struct connchain_i **ch, idx_t id,
+					struct connchain_buffer **b)
+{
+  struct connchain_buffer *buf = *b;
+  ssize_t i;
+
+  if (buf->saved_chain == NULL) { /* read data from chain if there is a room */
+    if ((*b)->error)
+      i = (*b)->error;
+    else if (buf->in.inbuf < sizeof(buf->in.buf)) {
+      if ((i = Connchain_Get(ch, id, &buf->in.buf[buf->in.bufptr],
+			     (sizeof(buf->in.buf) - buf->in.inbuf))) < 0)
+	(*b)->error = i;
+    } else
+      i = 0;
+    if (i > 0) {
+      buf->in.inbuf += i;
+      dprint(6, "openssl: got encrypted data from socket, size=%zd", i);
+    }
+  } else			/* not ready yet, try to pull saved buffers */
+    _s_check_saved_buffer(id, buf);
+  /* now process buffered data */
+  i = buf->in.inbuf - buf->in.bufptr;
+  if (i > 0) {
+    i = BIO_write(buf->in.bio, &buf->in.buf[buf->in.bufptr], i);
+    if (i <= 0) {		/* some error in BIO_write */
+      DBG("openssl: BIO_write error code %zd", i);
+    } else {			/* some data pushed into BIO */
+      if (buf->in.inbuf == buf->in.bufptr + i) /* all input consumed */
+	buf->in.bufptr = buf->in.inbuf = 0;
+      else
+	buf->in.bufptr += i;
+    }
+  }
+}
+
+/* send into saved_chain while in early state and reject data
+   kill saved chain as soon it's done */
+static ssize_t _ccfilter_S_send(struct connchain_i **ch, idx_t id, const char *str,
+				size_t *sz, struct connchain_buffer **b)
+{
+  struct connchain_buffer *buf = *b;
+  size_t so;
+  ssize_t i;
+
+  if (buf == NULL)		/* terminated */
+    return (E_NOSOCKET);
+  if (buf->saved_chain != NULL) { /* not ready yet, try to push saved buffers */
+    _s_check_saved_buffer(id, buf);
+    if (buf->saved_chain != NULL)
+      return (0);
+  }
+  if (str == NULL) {		/* asked to flush, we not call SSL here! */
+    if (buf->out.inbuf)
+      return (0);
+    return Connchain_Put (ch, id, str, sz); /* ask next link to flush then */
+  }
+  i = _ssl_try_send_buffers(ch, id, buf, TRUE); /* try to push buffers now */
+  if (i < 0)
+    return (i);
+  if (buf->out.inbuf >= (sizeof(buf->out.buf) - 16)) /* reserve 16 bytes */
+    return (0);			/* not ready now */
+  if (*sz == 0) {		/* a test */
+    if (!SSL_is_init_finished(buf->ssl))
+      return (0);
+    return Connchain_Put (ch, id, str, sz); /* bounce test to next link */
+  } else if (!SSL_is_init_finished(buf->ssl)) {
+    DBG("openssl: handshake is in progress");
+    SSL_do_handshake(buf->ssl);
+    _ssl_check_input_from_chain(ch, id, b); /* SSL may wait for data */
+    if (!SSL_is_init_finished(buf->ssl))
+      return (0);
+  }
+  /* ready to process input data */
+  i = SSL_write(buf->ssl, str, *sz);
+  if (i > 0)			/* some data were processed */
+    *sz -= i;
+  else if (i < 0) {		/* processing not available now */
+    DBG("openssl: SSL_write error code %d", SSL_get_error(buf->ssl, (int)i));
+    i = 0;
+  }
+  so = i;
+  if (buf->out.inbuf != 0) {	/* trying to push buffer again */
+    i = _ssl_try_send_buffers(ch, id, buf, TRUE);
+    if (i < 0)
+      return (i);
+  }
+  return (so);
+}
+
+/* termination includes saved chain if in early state */
+static ssize_t _ccfilter_S_recv(struct connchain_i **ch, idx_t id, char *str,
+				size_t sz, struct connchain_buffer **b)
+{
+  struct connchain_buffer *buf = *b;
+  ssize_t i;
+
+  if (buf == NULL)		/* terminated */
+    return E_NOSOCKET;
+  if (str == NULL)		/* termination request */
+    goto finish_filter;
+  if (sz == 0)			/* wrong call */
+    return (0);
+  if (id < 0) {			/* raw pull request */
+    if (buf->saved_chain != NULL)
+      _s_check_saved_buffer(id, buf);
+    if (buf->in.inbuf == 0)
+      return (Connchain_Get(ch, id, str, sz));
+    i = (buf->in.inbuf - buf->in.bufptr);
+    if (i > (ssize_t)sz)
+      i = sz;
+    memcpy(str, &buf->in.buf[buf->in.bufptr], i);
+    if (buf->in.bufptr + i == buf->in.inbuf)
+      buf->in.bufptr = buf->in.inbuf = 0;
+    else
+      buf->in.bufptr += i;
+    return (i);
+  }
+  if (!SSL_is_init_finished(buf->ssl)) { /* check for handshake */
+    SSL_do_handshake(buf->ssl);
+    i = _ssl_try_send_buffers(ch, id, buf, TRUE); /* SSL may have data to send */
+    if (i < 0)
+      return (i);
+  }
+  _ssl_check_input_from_chain(ch, id, b);
+  i = SSL_read(buf->ssl, str, sz);
+  if (i <= 0) {			/* some error in SSL_read */
+    /* DBG("openssl: SSL_read error code %d", SSL_get_error(buf->ssl, (int)i)); */
+    i = (*b)->error;		/* there might be error from connchain */
+  } else			/* got some data */
+    dprint(6, "openssl: decrypted data: [%-*.*s]", (int)i, (int)i, str);
+  if (i >= 0)
+    return (i);
+  ERROR("openssl: got %zd from connection chain, terminating", i);
+finish_filter:
+  if (buf->saved_chain != NULL && Connchain_Get(&buf->saved_chain, id, NULL, 0))
+    buf->saved_chain = NULL;
+  i = (*b)->error;
+  if (i == 0)
+    i = E_NOSOCKET;
+  _freesslbuff(b);
+  return (i);
+}
+
+static struct connchain_buffer *_make_buffer(struct peer_t *peer,
+	ssize_t (**recv) (struct connchain_i **, idx_t, char *, size_t, struct connchain_buffer **),
+	ssize_t (**send) (struct connchain_i **, idx_t, const char *, size_t *, struct connchain_buffer **))
+{
+  struct connchain_buffer *buf;
+
+  buf = safe_malloc (sizeof(struct connchain_buffer));
+  DBG("openssl: allocated buffer %p", buf);
+  *recv = &_ccfilter_S_recv;		/* init the structure */
+  *send = &_ccfilter_S_send;
+  buf->in.inbuf = buf->in.bufptr = buf->out.inbuf = buf->out.bufptr = 0;
+  buf->peer = peer;
+  buf->next = sslbuflist;		/* add it in list */
+  sslbuflist = buf;
+  buf->saved_chain = peer->connchain;	/* save existing connchain */
+  peer->connchain = NULL;		/* reset connchain */
+  Connchain_Grow(peer, 0);		/* reinit connchain on the peer */
+  /* init connection and buffers */
+  buf->ssl = SSL_new(ctx);
+  if (buf->ssl == NULL) {
+    // ... error
+  }
+  buf->in.bio = BIO_new(BIO_s_mem());
+  if (buf->in.bio == NULL) {
+    // ... error
+  }
+  BIO_set_mem_eof_return(buf->in.bio, -1); /* see: https://www.openssl.org/docs/crypto/BIO_s_mem.html */
+  buf->out.bio = BIO_new(BIO_s_mem());
+  if (buf->out.bio == NULL) {
+    // ... error
+  }
+  BIO_set_mem_eof_return(buf->out.bio, -1);
+  SSL_set_bio(buf->ssl, buf->in.bio, buf->out.bio);
+  buf->error = 0;
+  /* from now on all data should go through SSL */
+  return (buf);
+}
+
+BINDING_TYPE_connchain_grow(_ccfilter_S_init);
+static int _ccfilter_S_init (struct peer_t *peer,
+	ssize_t (**recv) (struct connchain_i **, idx_t, char *, size_t, struct connchain_buffer **),
+	ssize_t (**send) (struct connchain_i **, idx_t, const char *, size_t *, struct connchain_buffer **),
+	struct connchain_buffer **b)
+{
+  register struct connchain_buffer *buf;
+
+  if (b == NULL)			/* this is a test */
+    return (1);
+  *b = buf = _make_buffer(peer, recv, send);
+  /* start handshake as server side */
+  SSL_set_accept_state(buf->ssl);
+  return (1);
+}
+
+BINDING_TYPE_connchain_grow(_ccfilter_s_init);
+static int _ccfilter_s_init (struct peer_t *peer,
+	ssize_t (**recv) (struct connchain_i **, idx_t, char *, size_t, struct connchain_buffer **),
+	ssize_t (**send) (struct connchain_i **, idx_t, const char *, size_t *, struct connchain_buffer **),
+	struct connchain_buffer **b)
+{
+  register struct connchain_buffer *buf;
+
+  if (b == NULL)			/* this is a test */
+    return (1);
+  *b = buf = _make_buffer(peer, recv, send);
+  /* start handshake as client side */
+  SSL_set_connect_state(buf->ssl);
+  return (1);
+}
+
+
+/*
+ * this function must receive signals:
+ *  S_TERMINATE - unload module,
+ *  S_REPORT - out state info to log,
+ *  S_REG - report/register anything we should have in config file.
+ */
+static iftype_t module_signal (INTERFACE *iface, ifsig_t sig)
+{
+  INTERFACE *tmp;
+  struct connchain_buffer *buf;
+  char *termreason = "module 'openssl' termination";
+
+  switch (sig) {
+  case S_TERMINATE:
+    UnregisterVariable("openssl-certificate-file");
+    UnregisterVariable("openssl-key-file");
+    Delete_Binding("connchain-grow", &_ccfilter_S_init, NULL);
+    Delete_Binding("connchain-grow", &_ccfilter_s_init, NULL);
+    if (ShutdownR == NULL)
+      ShutdownR = termreason;
+    while (sslbuflist) {		/* kill every SSL in progress */
+      tmp = sslbuflist->peer->iface;
+      Send_Signal(tmp->ift, tmp->name, S_TERMINATE);
+      Set_Iface(tmp);			/* it may have deferred termination */
+      while (Get_Request());
+      Unset_Iface();
+    }
+    Delete_Help("openssl");
+    if (ShutdownR == termreason)
+      ShutdownR = NULL;
+    /* free ctx */
+    SSL_CTX_free(ctx);
+    ctx = NULL;
+    /* deinit library */
+    ERR_remove_state(0);
+    ENGINE_cleanup();
+    CONF_modules_unload(1);
+    ERR_free_strings();
+    EVP_cleanup();
+    sk_SSL_COMP_free(SSL_COMP_get_compression_methods());
+    CRYPTO_cleanup_all_ex_data();
+    break;
+  case S_REPORT:
+    tmp = Set_Iface(iface);
+    if ((buf = sslbuflist)) do {
+	if (buf->peer->dname && *buf->peer->dname)
+	  New_Request(tmp, F_REPORT, "OpenSSL link: used on peer %s.",
+		      buf->peer->dname);
+	else
+	  New_Request(tmp, F_REPORT, "OpenSSL link: used on nonamed peer (%hd).",
+		      buf->peer->socket);
+	buf = buf->next;
+      } while (buf);
+    else
+      New_Request(tmp, F_REPORT, "Module openssl: not used.");
+    Unset_Iface();
+    break;
+  case S_REG:
+    Add_Request (I_INIT, "*", F_REPORT, "module openssl");
+    break;
+  case S_TIMEOUT:
+    /* delayed init */
+    if (_initialized) {
+      Add_Request (I_LOG, "*", F_WARN, "openssl: stray S_TIMEOUT signal to module!");
+      break;				/* ignore it */
+    }
+    _initialized = TRUE;
+    // SSL_CTX_set_cipher_list(ctx, "ALL:!ADH:!LOW:!EXP:!MD5:@STRENGTH");
+    // SSL_CTX_set_verify(ctx, SSL_VERIFY_PEER, &_no_verify);
+    // SSL_CTX_set_tlsext_use_srtp(ctx, "SRTP_AES128_CM_SHA1_80");
+    if (ssl_certificate_file[0] == '\0') {
+      DBG("OpenSSL: using default certificates");
+      SSL_CTX_set_default_verify_paths(ctx);
+    } else if (SSL_CTX_use_certificate_file(ctx, ssl_certificate_file, SSL_FILETYPE_PEM) <= 0) {
+      ERROR("OpenSSL init failed: CTX_use_certificate_file: %s",
+	    ERR_error_string(ERR_get_error(), NULL));
+      goto _cleanup;
+    }
+    if (ssl_key_file[0] == '\0') {
+      WARNING("OpenSSL: no key file is set, server setup will not work");
+    } else if (SSL_CTX_use_PrivateKey_file(ctx, ssl_key_file, SSL_FILETYPE_PEM) <= 0) {
+      ERROR("OpenSSL init failed: CTX_use_PrivateKey_file: %s",
+	    ERR_error_string(ERR_get_error(), NULL));
+      goto _cleanup;
+    } else if (!SSL_CTX_check_private_key(ctx)) {
+      ERROR("OpenSSL init failed: CTX_check_private_key: %s",
+	    ERR_error_string(ERR_get_error(), NULL));
+_cleanup:
+      iface->ift |= I_FINWAIT;		/* schedule suicide */
+    }
+    break;
+  default: ;
+  }
+  return (0);
+}
+
+/*
+ * this function called when you load a module.
+ * Input: parameters string args.
+ * Returns: address of signals receiver function, NULL if not loaded.
+ */
+SigFunction ModuleInit (char *args)
+{
+  CheckVersion;
+  /* lib init */
+  SSL_library_init();
+  SSL_load_error_strings();
+  ERR_load_BIO_strings();
+  OpenSSL_add_all_algorithms();
+  /* init ctx */
+  ctx = SSL_CTX_new(SSLv23_method());
+  if (!ctx) {
+    ERROR("OpenSSL init failed: CTX_new: %s", ERR_error_string(ERR_get_error(), NULL));
+    return (NULL);
+  }
+  _initialized = FALSE;
+  RegisterString("openssl-certificate-file", ssl_certificate_file,
+		 sizeof(ssl_certificate_file), 0);
+  RegisterString("openssl-key-file", ssl_key_file, sizeof(ssl_key_file), 0);
+  Add_Help("openssl");
+  Add_Binding("connchain-grow", "S", 0, 0, &_ccfilter_S_init, NULL);
+  Add_Binding("connchain-grow", "s", 0, 0, &_ccfilter_s_init, NULL);
+  /* schedule init */
+  NewTimer(I_MODULE, "openssl", S_TIMEOUT, 1, 0, 0, 0);
+  return (&module_signal);
+}
+#else
+SigFunction ModuleInit (char *args)
+{
+  ERROR ("Cannot use OpenSSL, recompile package to get module 'openssl' working.");
+  return (NULL);
+}
+#endif /* USE_OPENSSL */
