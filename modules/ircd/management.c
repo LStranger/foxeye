8c4602084decd45ecacbfb8fddcc18bd9dd63daf
Andriy Grytsenko <andrej@rep.kiev.ua>
2017-02-11 15:42:46 +0200

    ircd: add command "-hub" to negate "+hub".

--- modules/ircd/management.c
+++ modules/ircd/management.c
@@ -61,7 +61,62 @@ static int ss_ircd_phub (struct peer_t *dcc, INTERFACE *srv, char *args)
 }
 
 		/* .-hub server mask */
-//remove all masks matched
+BINDING_TYPE_ss_ (ss_ircd_mhub);
+static int ss_ircd_mhub (struct peer_t *dcc, INTERFACE *srv, char *args)
+{
+  char *c, *r , *f, *ff, *fn, *fc, *fp;
+  struct clrec_t *u;
+  int ret = 1;
+
+  if (!args)
+    return 0;				/* need exactly 2 args */
+  r = gettoken (args, &c);
+  if (!*r)
+    return 0;				/* need exactly 2 args */
+  u = Lock_Clientrecord (args);
+  if (!u)
+  {
+    New_Request (dcc->iface, 0, "Server %s not found", args);
+    if (*r)
+      *c = ' ';
+    return 0;
+  }
+  args = safe_strdup(Get_Field(u, NULL, NULL)); /* unalias the name */
+  for (ff = fp = f = safe_strdup(Get_Field(u, "hub", NULL)); f && *f; )
+  {
+    fn = gettoken(f, &fc);
+    while (match(f, r) >= 0)
+    {
+      DBG("ircd:-hub: %s matches %s, skipping it", f, r);
+      f = fn;
+      if (*f == '\0')
+	break;
+      fn = gettoken(f, NULL);
+    }
+    if (*f)
+    {
+      DBG("ircd:-hub: keeping %s", f);
+      if (fp != ff)
+	*fp++ = ' ';
+      if (f != fp)
+	memmove(fp, f, strlen(f));
+      fp += strlen(f);
+      f = fn;
+    }
+  }
+  if (ff)
+    ret = Set_Field(u, "hub", ff, 0);
+  Unlock_Clientrecord (u);
+  FREE(&ff);
+  if (ret)
+    New_Request (dcc->iface, 0, "Removed hub mask \"%s\" from %s.", r, args);
+  else
+    New_Request (dcc->iface, 0, "Failed to remove hub mask \"%s\" from %s.", r, args);
+  FREE(&args);
+  *c = ' ';
+  return 1;
+}
+
 		/* .class name [parameters] */
 //either show, or change class parameters
 		/* .bounce class [ip [port]] */
@@ -72,13 +127,14 @@ static int ss_ircd_phub (struct peer_t *dcc, INTERFACE *srv, char *args)
 /* -- common module interface --------------------------------------------- */
 void ircd_management_proto_end (void)
 {
-  Delete_Binding ("dcc", &ss_ircd_phub, NULL);
+  Delete_Binding ("ss-ircd", &ss_ircd_phub, NULL);
+  Delete_Binding ("ss-ircd", &ss_ircd_mhub, NULL);
 }
 
 void ircd_management_proto_start (void)
 {
   Add_Binding ("ss-ircd", "+hub", U_MASTER, U_MASTER, &ss_ircd_phub, NULL);
-  //Add_Binding ("ss-ircd", "-hub", U_MASTER, U_MASTER, &ss_ircd_mhub, NULL);
+  Add_Binding ("ss-ircd", "-hub", U_MASTER, U_MASTER, &ss_ircd_mhub, NULL);
   //Add_Binding ("ss-ircd", "class", U_MASTER, U_MASTER, &ss_ircd_class, NULL);
   //Add_Binding ("ss-ircd", "bounce", U_OP, U_OP, &ss_ircd_bounce, NULL);
   //Add_Binding ("ss-ircd", "history", U_OP, U_HALFOP, &ss_ircd_history, NULL);
