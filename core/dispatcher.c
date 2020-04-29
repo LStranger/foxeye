08c30601a201be2591b3087152e2995431e12b4d
Andriy Grytsenko <andrej@rep.kiev.ua>
2020-04-30 00:16:26 +0300

    Add a safeguard against failed conversion when sending requests.

--- core/dispatcher.c
+++ core/dispatcher.c
@@ -393,6 +393,7 @@ static void vsadd_request (ifi_t *to, iftype_t ift, const char *mask,
   cur->a.mask_if = ift;
   cur->a.from = &Current->a;
   cur->a.flag = flag;
+  cur->a.string[0] = '\0';
   vsnprintf (cur->a.string, sizeof(cur->a.string), fmt, ap);
   if (!(flag & F_DEBUG))
   {
@@ -457,6 +458,11 @@ static void vsadd_request (ifi_t *to, iftype_t ift, const char *mask,
 	  s = strlen (cur->a.string);
 	  s = Undo_Conversion (conv, &ch, sizeof(req->a.string) - 1,
 			       cur->a.string, &s); /* ignore unconverted size */
+	  if (ch != req->a.string)
+	  {
+	    DBG("dispatcher:vsadd_request: ERROR on conversion, copy instead");
+	    strfcpy(req->a.string, cur->a.string, sizeof(req->a.string));
+	  }
 	  ch[s] = 0;
 	  if (lastdebuglog)
 	  {
